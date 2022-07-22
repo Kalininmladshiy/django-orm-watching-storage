9a74a5c89e54ad157f17406a42ac7b3a7e4f8640
Максим Калинин <maksim.kalinin1@gmail.com>
2022-07-22 12:06:46 +0300

    Changed_view_files

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,41 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+import django
+import datetime
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    visits = Visit.objects.filter(passcard__passcode=passcode)
     this_passcard_visits = [
         {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
+            'entered_at': django.utils.timezone.localtime(visit.entered_at),
+            'duration': get_duration(visit),
+            'is_strange': is_visit_long(visit),
+        }
+        for visit in visits
     ]
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
+
+
+def is_visit_long(visit):
+    local_leave_time = django.utils.timezone.localtime(visit.leaved_at)
+    local_visit_time = django.utils.timezone.localtime(visit.entered_at)
+    duration = local_leave_time - local_visit_time
+    visit_time_minutes = int(duration.total_seconds() // 60)
+    return visit_time_minutes > 60
+
+
+def get_duration(visit):
+    local_leave_time = django.utils.timezone.localtime(visit.leaved_at)
+    local_visit_time = django.utils.timezone.localtime(visit.entered_at)
+    duration = local_leave_time - local_visit_time
+    duration_hours = int(duration.total_seconds() // 3600)
+    duration_minutes = int((duration.total_seconds() % 3600) // 60)
+    return f'{duration_hours} hours : {duration_minutes} minutes'
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,34 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+import django
+import datetime
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
+    visits = Visit.objects.filter(leaved_at=None)
     non_closed_visits = [
         {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': django.utils.timezone.localtime(visit.entered_at),
+            'duration': format_duration(get_duration(visit)),
+            }
+        for visit in visits
     ]
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
+
+
+def get_duration(visit):
+    current_time = django.utils.timezone.localtime()
+    local_visit_time = django.utils.timezone.localtime(visit.entered_at)
+    duration = current_time - local_visit_time
+    return duration
+
+
+def format_duration(duration):
+    hours = int(duration.total_seconds() // 3600)
+    minutes = int((duration.total_seconds() % 3600) // 60)
+    return f'{hours}:{minutes}'
