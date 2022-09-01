08456da0209f8e8ee29aa83cbfa59cedd3440f7d
Максим Калинин <maksim.kalinin1@gmail.com>
2022-09-01 14:28:27 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -9,11 +9,12 @@ import datetime
 def storage_information_view(request):
     visits = Visit.objects.filter(leaved_at__isnull=True)
     non_closed_visits = []
-    params = {}
     for visit in visits:
-        params['who_entered'] = visit.passcard.owner_name
-        params['entered_at'] = django.utils.timezone.localtime(visit.entered_at)
-        params['duration'] = get_format_duration(get_duration(visit, django.utils.timezone.localtime()))
+        params = {
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': django.utils.timezone.localtime(visit.entered_at),
+            'duration': get_format_duration(get_duration(visit, django.utils.timezone.localtime())),
+         }
         non_closed_visits.append(params)
         params = {}
     context = {
